8a764485b06a958f6f1f9ca976ef0ed7e8fa7361
lsy3709 <lsy3709@naver.com>
2026-02-19 14:21:20 +0900

    5교시, 스프링 MVC 예외처리 기본 3, 404 처리작업 확인

--- src/main/java/com/busanit501/jsp_server_project1/springex_0213_keep/controller/exception/CommonExceptionAdvice.java
+++ src/main/java/com/busanit501/jsp_server_project1/springex_0213_keep/controller/exception/CommonExceptionAdvice.java
@@ -1,9 +1,12 @@
 package com.busanit501.jsp_server_project1.springex_0213_keep.controller.exception;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Arrays;
 
@@ -54,6 +57,8 @@ public class CommonExceptionAdvice {
     // return "custom404"; -> 해당 화면을 의미.
     // /WEB-INF/views/custom404.jsp
     // 화면을 리턴함.
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String notFound() {
         return "custom404";
     }
