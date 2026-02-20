f52f254f3c8a834981f8ba0a8742b1edf5129317
lsy3709 <lsy3709@naver.com>
2026-02-20 16:38:56 +0900

    7교시, 목록조회 작업 중, 서비스 작업 까지 했고, 단위테스트 부터 이어서 진행하기

--- src/main/java/com/busanit501/jsp_server_project1/springex_new_0219_keep/service/TodoServiceImpl.java
+++ src/main/java/com/busanit501/jsp_server_project1/springex_new_0219_keep/service/TodoServiceImpl.java
@@ -8,6 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @Log4j2
 @RequiredArgsConstructor
@@ -30,5 +33,13 @@ public class TodoServiceImpl implements TodoService{
         log.info("서비스 작업: insert 기능 변환된 todoVO : " + todoVO);
         todoMapper.insert(todoVO);
 
+    }
+
+    @Override
+    public List<TodoDTO> getAll() {
+        List<TodoDTO> dtoList = todoMapper.selectAll().stream()
+                .map(vo -> modelMapper.map(vo, TodoDTO.class))
+                .collect(Collectors.toList());
+        return dtoList;
     }
 }
